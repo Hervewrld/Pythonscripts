0b49302f92b7030cf302fe4719e3501992d96d5a
Hervewrld <133717283+Hervewrld@users.noreply.github.com>
2025-08-25 21:11:35 +0000

    s10

---
+++ Section 1/script10.py
@@ -0,0 +1,40 @@
+# Boolean datatype
+
+is_server_up = True
+if is_server_up == True:
+    print("The server is running")
+else:
+    print("The server is down")
+
+
+# Greater than
+
+a = 10
+b = 20
+
+is_equal = a == b  # comparison for equality
+is_greater = a < b # comparison for greater than
+
+print(is_equal)
+print(is_greater)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
